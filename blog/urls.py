c872c4e297aa3d8ad2a2f433fd4a12fa011fcd6c
mofi <mofbiskra83@gmail.com>
2020-11-10 13:01:33 +0100

    add post_list and post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path 
 
-from .views import post_list, post_detail 
+from .views import post_list, post_detail,post_create,post_edit
 
 app_name = 'blog'
 
 urlpatterns = [
     
     path('', post_list,name='post_list'),
-    path('<int:id>',post_detail,name='post_detail')
+    path('<int:id>',post_detail,name='post_detail'),
+    path('<int:id>/edit',post_edit,name='post_edit'),
+    path('new',post_create,name='new_post'),
+    
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render 
+from django.shortcuts import render,redirect,reverse 
 from django.forms.models import model_to_dict
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -10,6 +11,29 @@ def post_list(request):
     post_list = Post.objects.all()
 
     return render (request , 'blog/post-list.html' ,{'post_list':post_list,})
+
 def post_detail(request,id):
     post = Post.objects.get(id=id)
     return render(request,'blog/post_detail.html',{'signal_post':post})
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('blog:post_list'))
+    else :
+        form = PostForm()
+    return render(request,'blog/post_create.html',{'form' : form})
+
+def post_edit(request,id):  
+    post = Post.objects.get(id=id)    
+    if request.method == 'POST':
+        form = PostForm(request.POST,instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('blog:post_detail',kwargs={'id': post.id}))
+    else :
+            form = PostForm(instance=post)
+    return render(request,'blog/post_edit.html',{'form' : form})
+         
